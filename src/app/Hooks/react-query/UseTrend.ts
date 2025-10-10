8ec707c44618b2527f3674d7c4cd8250f88298f0
Mahdi <mahd8767trfx@gmail.com>
2025-10-10 23:41:20 +0330

    get trends from route

---
+++ src/app/Hooks/react-query/UseTrend.ts
@@ -0,0 +1,13 @@
+import { CryptoAPIResponse } from "@/app/interfaces/trend";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export function UseTrend(){
+    return useQuery<CryptoAPIResponse[]>({
+        queryKey:['trend'],
+        queryFn: async ()=> {
+            const response = await axios.get('http://localhost:3000/api/crypto/trend')
+            return JSON.parse(JSON.stringify(response.data))
+        }
+    })
+}
