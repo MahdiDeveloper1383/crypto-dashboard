627b6b111d043daefbc992c4d9a3da388309f547
Mahdi <mahd8767trfx@gmail.com>
2025-10-17 01:11:17 +0330

    set card for Coinstable

---
+++ src/Components/Cards/CoinstableCard.tsx
@@ -0,0 +1,39 @@
+import { CoinMarket } from '@/Interfaces/crypto/market'
+import Image from 'next/image'
+import React from 'react'
+
+export default function CoinstableCard({coin,index}:{coin:CoinMarket,index:number}) {
+  return (
+     <tr
+                  key={coin.id}
+                  className=" hover:bg-gray-50 transition text-xl font-normal"
+                >
+                  <td className="px-4 py-3 text-center">{index + 1}</td>
+                  <td className="px-4 py-3 flex items-center gap-2">
+                    <Image
+                      src={coin.image}
+                      className="rounded-full"
+                      alt="coin_symbol"
+                      width={30}
+                      height={30}
+                    />
+                    <span className="font-semibold text-2xl">{coin.name} </span>
+                    <span className="uppercase text-gray-600 text-xs">
+                      {coin.symbol}
+                    </span>
+                  </td>
+                  <td className="px-4 py-3">${coin.current_price.toFixed(2)}</td>
+                  <td
+                    className={`px-4 py-3 ${
+                      coin.market_cap_change_percentage_24h < 0
+                        ? "text-red-600"
+                        : "text-green-400"
+                    }`}
+                  >
+                    %{coin.market_cap_change_percentage_24h?.toFixed(2) ?? "0.0"}
+                  </td>
+                  <td className="px-4 py-3">${coin.market_cap}</td>
+                  <td className="px-4 py-3">${coin.total_volume}</td>
+                </tr>
+  )
+}
--- src/Components/markets/MarketCointable.tsx
+++ src/Components/markets/MarketCointable.tsx
@@ -4,6 +4,7 @@ import { usePagination } from "@/Hooks/UsePagition";
 import { UseMarket } from "@/react-query/UseMarket";
 import Image from "next/image";
 import React from "react";
+import CoinstableCard from "../Cards/CoinstableCard";
 
 export default function MarketCointable() {
   const { data: Coins } = UseMarket();
@@ -80,37 +81,7 @@ export default function MarketCointable() {
         </thead>
         <tbody>
           {currentCoins?.map((coin, index) => (
-            <tr
-              key={coin.id}
-              className=" hover:bg-gray-50 transition text-xl font-normal"
-            >
-              <td className="px-4 py-3 text-center">{index + 1}</td>
-              <td className="px-4 py-3 flex items-center gap-2">
-                <Image
-                  src={coin.image}
-                  className="rounded-full"
-                  alt="coin_symbol"
-                  width={30}
-                  height={30}
-                />
-                <span className="font-semibold text-2xl">{coin.name} </span>
-                <span className="uppercase text-gray-600 text-xs">
-                  {coin.symbol}
-                </span>
-              </td>
-              <td className="px-4 py-3">${coin.current_price.toFixed(2)}</td>
-              <td
-                className={`px-4 py-3 ${
-                  coin.market_cap_change_percentage_24h < 0
-                    ? "text-red-600"
-                    : "text-green-400"
-                }`}
-              >
-                %{coin.market_cap_change_percentage_24h?.toFixed(2) ?? "0.0"}
-              </td>
-              <td className="px-4 py-3">${coin.market_cap}</td>
-              <td className="px-4 py-3">${coin.total_volume}</td>
-            </tr>
+           <CoinstableCard coin={coin} index={index} key={coin.id}/>
           ))}
         </tbody>
       </table>
