7635cdc496543c9bc65a646faf3062b961e9243e
Mahdi <mahd8767trfx@gmail.com>
2025-10-19 12:44:11 +0330

    Add Footer and Header for coin page

--- src/app/coins/[id]/page.tsx
+++ src/app/coins/[id]/page.tsx
@@ -1,20 +1,26 @@
 'use client'
 import React from 'react'
 import { UseCoin } from '@/react-query/UseCoin'
+import Header from '@/Components/layout/Header'
+import Footer from '@/Components/layout/Footer'
 
 export default function Coin({ params }: { params: Promise<{ id: string }> }) {
   const { id } = React.use(params)
 
-  const { data, isLoading, error } = UseCoin({ coin: id, currency: 'usd' })
+  const { data:coin, isLoading, error } = UseCoin({ coin: id, currency: 'usd' })
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error loading data</div>
 
   return (
+    <React.Fragment>
+    <Header/>
     <div>
-      <h2>{data?.id}</h2>
-      <p>{data?.name}</p>
-      <p>{data?.symbol}</p>
+      <h2>{coin.id}</h2>
+      <p>{coin.name}</p>
+      <p>{coin.symbol}</p>
     </div>
+    <Footer/>
+    </React.Fragment>
   )
 }
