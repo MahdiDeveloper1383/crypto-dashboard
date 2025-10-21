1c6d3517ddb180c2186e4dabbb62e2787451db5c
Mahdi <mahd8767trfx@gmail.com>
2025-10-22 00:38:05 +0330

    set news page

---
+++ src/app/news/page.tsx
@@ -0,0 +1,12 @@
+import Footer from '@/Components/layout/Footer'
+import Header from '@/Components/layout/Header'
+import React from 'react'
+
+export default function News() {
+  return (
+    <React.Fragment>
+        <Header/>
+        <Footer/>
+    </React.Fragment>
+  )
+}
