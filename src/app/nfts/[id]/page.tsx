466c2a8a2b20f2b7d8888d018f05c453582cff2d
Mahdi <mahd8767trfx@gmail.com>
2025-10-24 19:01:48 +0330

    set page for each nft

---
+++ src/app/nfts/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NFT({params}:{params:Promise<{id:string}>}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/react-query/UseNftList.ts
+++ src/react-query/UseNftList.ts
@@ -4,9 +4,9 @@ import axios from "axios";
 
 export default function UseNftList(){
     return useQuery<INftList[]>({
-        queryKey:['ntfs'],
+        queryKey:['ntfs-list'],
         queryFn:async()=>{
-            const response = await axios.get('http://localhost:3000/api/crypto/nfts')
+            const response = await axios.get('http://localhost:3000/api/crypto/nfts-list')
             return JSON.parse(JSON.stringify(response.data))
         }
     })
